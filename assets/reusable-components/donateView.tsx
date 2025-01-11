387c07f84b24342908fff2a7352ebd0aa87307d2
Teja Jonnalagadda <tjonnalagadda@verimatrix.com>
2025-01-12 01:35:52 +0530

    updated donate view

--- assets/reusable-components/donateView.tsx
+++ assets/reusable-components/donateView.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Linking } from 'react-native';
 import { Button, Checkbox } from 'react-native-paper';
 import { Formik, Field } from 'formik';
 import CustomInput from '@/assets/reusable-components/customInput';
@@ -9,158 +9,160 @@ import { BodyTextCheckbox } from './bodyText';
 import { HeaderText } from './headerText';
 
 const loginValidationSchema = yup.object().shape({
+  fName: yup.string().required('First Name is required'),
+  lName: yup.string().required('Last Name is required'),
   email: yup
     .string()
     .email("Please enter valid email")
     .required('Email Address is Required'),
-  password: yup
+  phone: yup
     .string()
-    .min(8, ({ min }) => `Password must be at least ${min} characters`)
-    .required('Password is required'),
-})
+    .required('Phone Number is Required')
+    .matches(/^[0-9]{10}$/, 'Phone number must be 10 digits'),
+  amount: yup
+    .number()
+    .required('Amount is required')
+    .positive('Amount must be greater than 0')
+    .min(1, 'Minimum donation is $1'),
+  description: yup.string().required('Description is required'),
+});
 
-function DonateView () {
+function DonateView() {
   const [checked, setChecked] = React.useState(false);
+
+  const handlePayPalRedirect = () => {
+    // Dummy PayPal URL for testing
+    const dummyPayPalURL = 'https://www.paypal.com/donate?hosted_button_id=XYZ123';
+    Linking.openURL(dummyPayPalURL).catch((err) => console.error("Failed to open PayPal link", err));
+  };
+
   return (
-    <View style={{flex: 1,marginBottom: 10, marginTop: 10, padding: 10, borderRadius: 15, backgroundColor: 'white'}}>
+    <View style={styles.container}>
       <Formik
-          validationSchema={loginValidationSchema}
-          initialValues={{
-              fName: '',
-              lName: '',
-              email: '',
-              phoneNumber: '',
-              amount: '',
-              description:'',
-          }}
-          onSubmit={values => console.log(values)}
-          style={{}}
+        validationSchema={loginValidationSchema}
+        initialValues={{
+          fName: '',
+          lName: '',
+          email: '',
+          phone: '',
+          amount: '',
+          description: '',
+        }}
+        onSubmit={(values) => console.log(values)}
       >
-      {({
-          handleSubmit, isValid
-      }) => (
-        <View>
-          <Field
-            component={CustomInput}
-            name="fName"
-            label="First Name"
-            mode="flat"
-          />
-          <Field
-            component={CustomInput}
-            name="lName"
-            label="Last Name"
-            mode="flat"
-          />
-          <Field
-            component={CustomInput}
-            name="email"
-            label="Email"
-            mode="flat"
-          />
-          <Field
-            component={CustomInput}
-            name="phone"
-            label="Phone Number"
-            placeholder="1234567890"
-            mode="flat"
-          />
-          <Field
-            component={CustomInput}
-            name="amount"
-            label="Amount"
-            placeholder="50"
-            mode="flat"
-          />
-          <Field
-            component={CustomInput}
-            name="description"
-            label="Description"
-            multiline={true}
-            numberOfLines={10}
-            mode="flat"
-          />
-          <View style={{width: '100%', position: 'relative'}}>
-            <Checkbox
-              status={checked ? 'checked' : 'unchecked'}
-              onPress={() => {
-                setChecked(!checked);
-              }}
-              style={{position: 'absoulte', left: 5}}
+        {({ handleSubmit, isValid }) => (
+          <View style={styles.formContainer}>
+            <Field
+              component={CustomInput}
+              name="fName"
+              label="First Name"
+              mode="outlined"
+              style={styles.input}
             />
-            <BodyTextCheckbox style={{width: '89%', position: 'absolute', right: 0, textAlign: 'left'}}>Telangana American Telugu Association Section 501(c) (3) Non-Profit organization, All donations are deemed tax-deductible absent any limitations on deductible applicable to a particular taxpayer. No goods or services were provided in exchange for your contribution.</BodyTextCheckbox>
+            <Field
+              component={CustomInput}
+              name="lName"
+              label="Last Name"
+              mode="outlined"
+              style={styles.input}
+            />
+            <Field
+              component={CustomInput}
+              name="email"
+              label="Email"
+              mode="outlined"
+              style={styles.input}
+            />
+            <Field
+              component={CustomInput}
+              name="phone"
+              label="Phone Number"
+              placeholder="1234567890"
+              mode="outlined"
+              style={styles.input}
+            />
+            <Field
+              component={CustomInput}
+              name="amount"
+              label="Amount"
+              placeholder="50"
+              mode="outlined"
+              style={styles.input}
+              keyboardType="numeric"
+            />
+            <Field
+              component={CustomInput}
+              name="description"
+              label="Description"
+              multiline={true}
+              numberOfLines={4}
+              mode="outlined"
+              style={styles.input}
+            />
+
+            <View style={styles.checkboxContainer}>
+              <Checkbox
+                status={checked ? 'checked' : 'unchecked'}
+                onPress={() => setChecked(!checked)}
+              />
+              <BodyTextCheckbox style={styles.checkboxText}>
+                Telangana American Telugu Association Section 501(c)(3) Non-Profit organization. All donations are deemed tax-deductible absent any limitations on deductible applicable to a particular taxpayer.
+              </BodyTextCheckbox>
+            </View>
+
+            <Button
+              onPress={handlePayPalRedirect} // Dummy PayPal button integration
+              mode="contained"
+              style={styles.donateButton}
+              disabled={!isValid} // Button is disabled if the form is invalid
+            >
+              Donate
+            </Button>
           </View>
-          <Button
-            onPress={() =>handleSubmit}
-            mode="contained"
-            style={{marginTop: 100}}
-          >
-          Donate
-          </Button>
-        </View>
-      )}
+        )}
       </Formik>
     </View>
-)};
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-      marginTop: 30
-    },
-    error: {
-      color: 'red'
-    },
-    inputContainer: {
-      flex:1,
-      padding: 5,
-      marginBottom: 10
-    },
-    text: {
-      color: '#fff',
-    },
-    image: {
-      width: '100%',
-      height: '100%',
-      resizeMode: 'repeat',
-    },
-    inputs: {
-      backgroundColor: '#fff',
-      borderRadius: 10,
-      padding: 10,
-      borderWidth: 1,
-      borderColor: 'grey'
-    },
-    shadowProp: {
-      shadowColor: 'black',
-      shadowOffset: {width: -2, height: 4},
-      shadowOpacity: 0.2,
-      shadowRadius: 3,
-    },
-    separator: {
-      marginVertical: 8,
-      width: '25%'
-    },
-    button: {
-      paddingHorizontal: 8,
-      paddingVertical: 6,
-      borderRadius: 4,
-      backgroundColor: 'oldlace',
-      alignSelf: 'flex-start',
-      marginHorizontal: '1%',
-      marginBottom: 15,
-      minWidth: '48%',
-      textAlign: 'center',
-    },
-    selected: {
-      color: 'white',
-      borderWidth: 0,
-    },
-    buttonLabel: {
-      textAlign: 'center'
-    },
-    selectedLabel: {
-      color: 'white',
-    },
-  });
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f4f4f4',
+    borderRadius: 15,
+  },
+  formContainer: {
+    backgroundColor: 'white',
+    borderRadius: 15,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 15,
+  },
+  checkboxText: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 12,
+    color: '#555',
+  },
+  donateButton: {
+    marginTop: 20,
+    backgroundColor: '#f1c40f',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+});
 
 export default DonateView;
